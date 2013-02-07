68b561a0505e538bbb8a29411fc56931e8dc309a
Hashcode <hashcode0f@gmail.com>
2013-02-06 16:31:49 -0800

    update device build
    Change-Id: I3d0138a48fa9613723fcef7ef848ed66dbdfdd35

--- device.mk
+++ device.mk
@@ -10,28 +10,12 @@ PRODUCT_AAPT_CONFIG := large hdpi xhdpi
 
 # Rootfs
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/init.bowser.rc:root/init.bowser.rc
-
-# Prebuilts /system/bin
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/bin/provision_device:/system/bin/provision_device \
-    $(DEVICE_FOLDER)/prebuilt/bin/provisioning_client:/system/bin/provisioning_client \
-    $(DEVICE_FOLDER)/prebuilt/bin/start_smc.sh:/system/bin/start_smc.sh \
-    $(DEVICE_FOLDER)/prebuilt/bin/atmel_touch.sh:/system/bin/atmel_touch.sh
-
-# Remove ducati for now until I fix ducati load crash
-# Prebuilt firmware
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/firmware/maxtouch.cfg:/system/vendor/firmware/maxtouch.cfg \
-    $(DEVICE_FOLDER)/firmware/maxtouch.fw:/system/vendor/firmware/maxtouch.fw \
-    $(DEVICE_FOLDER)/firmware/mxt-fw20.enc:/system/vendor/firmware/mxt-fw20.enc \
-    $(DEVICE_FOLDER)/firmware/smc_pa.ift:/system/vendor/firmware/smc_pa.ift \
-    $(DEVICE_FOLDER)/firmware/smc_pa.ift.version:/system/vendor/firmware/smc_pa.ift.version \
-    $(DEVICE_FOLDER)/firmware/ducati-m3.bin:/system/vendor/firmware/ducati-m3.bin.bak
+    $(DEVICE_FOLDER)/init.bowser.rc:root/init.bowser.rc \
+    $(DEVICE_FOLDER)/init.bowser.usb.rc:root/init.bowser.usb.rc
 
 # Prebuilts /system/etc
 PRODUCT_COPY_FILES += \
-    $(COMMON_FOLDER)/audio-bowser/tate.xml:/system/etc/sound/tate \
+    $(DEVICE_FOLDER)/prebuilt/etc/tate.xml:/system/etc/sound/tate \
 
 # Prebuilt /system/usr
 PRODUCT_COPY_FILES += \
@@ -39,15 +23,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/usr/idc/AtmelTouch.idc:system/usr/idc/AtmelTouch.idc \
     $(DEVICE_FOLDER)/prebuilt/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl\
     $(DEVICE_FOLDER)/prebuilt/usr/keylayout/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl
-
-# Prebuilt /system/lib
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/lib/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libinvensense_hal.so:system/lib/libinvensense_hal.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libmllite.so:system/lib/libmllite.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libmlplatform.so:system/lib/libmlplatform.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libmplmpu.so:system/lib/libmplmpu.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libril-lab126qmi.so:/system/lib/libril-lab126qmi.so \
 
 # Recovery
 PRODUCT_COPY_FILES += \
@@ -62,3 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.lab126.chargeprotect=1 \
     ro.nf.profile=2 \
     ro.nf.level=512 \
+
+$(call inherit-product-if-exists, vendor/invensense/tate/device-partial.mk)
+$(call inherit-product-if-exists, vendor/imgtec/amazon-common/device-partial-540.mk)
+$(call inherit-product-if-exists, vendor/amazon/tate/tate-vendor.mk)
