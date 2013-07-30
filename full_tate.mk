8010522d0269f6a552a5d5a97e513ad3712b3efa
Hashcode <hashcode0f@gmail.com>
2013-07-30 12:20:40 -0700

    cleanup .mk files

--- full_tate.mk
+++ full_tate.mk
@@ -12,11 +12,8 @@ PRODUCT_PACKAGES += \
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/amazon/tate/device.mk)
 
-# Discard inherited values and use our own instead.
 PRODUCT_NAME := full_tate
 PRODUCT_DEVICE := tate
 PRODUCT_BRAND := amazon
