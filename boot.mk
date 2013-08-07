e53ee8b5c997e7bf442d3b90c832a06212c2380e
Hashcode <hashcode0f@gmail.com>
2013-08-07 14:45:33 -0700

    fix boot.mk for phablet-saucy build

--- boot.mk
+++ boot.mk
@@ -10,17 +10,13 @@ define make_stack
   for i in $$(seq 1024) ; do echo -ne $(BOWSER_BOOT_ADDRESS) >>$(1) ; done
 endef
 
-INTERNAL_BOOTIMAGE_ARGS2 := \
-	--kernel $(INSTALLED_KERNEL_TARGET) \
-	--ramdisk $(COMMON_FOLDER)/initrd.img-touch \
-	--cmdline "$(BOARD_KERNEL_CMDLINE)" \
-	--base $(BOARD_KERNEL_BASE) \
-	--pagesize $(BOARD_KERNEL_PAGESIZE)
+INTERNAL_BOOTIMAGE_ARGS += $(INTERNAL_BOOTIMAGE_ARGS_COMMON)
+INTERNAL_BOOTIMAGE_ARGS_ANDROID += $(INTERNAL_BOOTIMAGE_ARGS_COMMON)
 
-$(INSTALLED_BOOTIMAGE_TARGET): \
-		$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOWSER_BOOTLOADER)
-	$(call pretty,"Making target Ubuntu-Touch boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS2) --output $@.tmp
+$(INSTALLED_BOOTIMAGE_TARGET_ANDROID): \
+		$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES_ANDROID) $(BOWSER_BOOTLOADER)
+	$(call pretty,"Target boot image for Android: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS_ANDROID) $(BOARD_MKBOOTIMG_ARGS) --output $@.tmp
 	$(hide) cat $(BOWSER_BOOT_CERT_FILE) $@.tmp >$@
 	$(hide) rm -f $@.tmp
 	$(call pretty,"Adding kindle specific u-boot for boot.img")
@@ -29,6 +25,21 @@ $(INSTALLED_BOOTIMAGE_TARGET): \
 	$(call make_stack,$(BOWSER_STACK_FILE))
 	$(hide) dd if=$(BOWSER_STACK_FILE) of=$@ bs=6519488 seek=1 conv=notrunc
 	$(hide) rm -f $(BOWSER_STACK_FILE)
+	@echo -e ${CL_CYN}"Made boot image for Android: $@"${CL_RST}
+
+$(INSTALLED_BOOTIMAGE_TARGET): \
+		$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOWSER_BOOTLOADER)
+	$(call pretty,"Target boot image for Ubuntu Touch: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.tmp2
+	$(hide) cat $(BOWSER_BOOT_CERT_FILE) $@.tmp2 >$@
+	$(hide) rm -f $@.tmp
+	$(call pretty,"Adding kindle specific u-boot for boot.img")
+	$(hide) dd if=$(BOWSER_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
+	$(call pretty,"Adding kindle specific payload to boot.img")
+	$(call make_stack,$(BOWSER_STACK_FILE))
+	$(hide) dd if=$(BOWSER_STACK_FILE) of=$@ bs=6519488 seek=1 conv=notrunc
+	$(hide) rm -f $(BOWSER_STACK_FILE)
+	@echo -e ${CL_CYN}"Made boot image for Ubuntu Touch: $@"${CL_RST}
 
 # XXX - While I'd like to do this check, currently it adds some reserve
 # that totally kills the deal.
@@ -36,16 +47,12 @@ $(INSTALLED_BOOTIMAGE_TARGET): \
 #		$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): \
-		$(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) \
-		$(BOWSER_BOOTLOADER)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.tmp
+		$(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) $(BOWSER_BOOTLOADER)
+	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@.tmp
 	$(hide) cat $(BOWSER_BOOT_CERT_FILE) $@.tmp >$@
 	$(hide) rm -f $@.tmp
 	$(call pretty,"Adding kindle specific u-boot for recovery.img")
 	$(hide) dd if=$(BOWSER_BOOTLOADER) of=$@ bs=8117072 seek=1 conv=notrunc
-# XXX - While I'd like to do this check, currently it adds some reserve
-# that totally kills the deal.
-#	$(hide) $(call assert-max-image-size,$@,\
-#		$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
 
