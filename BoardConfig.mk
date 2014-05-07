ced0bd8b70a0e04c57e532353ca59bf281e1e2cf
Hashcode <hashcode0f@gmail.com>
2014-05-07 13:14:37 -0700

    tate: releasetools handled in bowser-common
    Change-Id: Iec4e4558586e9d807cf1551eb82c41f23c88d002

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,10 +43,6 @@ TARGET_KERNEL_MODULES += SGX_MODULES TOUCH_MODULES
 # OTA Packaging / Bootimg creation
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_FOLDER)/boot.mk
 
-# hack the ota
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./$(DEVICE_FOLDER)/releasetools/bowser_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./$(DEVICE_FOLDER)/releasetools/bowser_img_from_target_files
-
 # Recovery/TWRP Config
 TARGET_RECOVERY_FSTAB = $(DEVICE_FOLDER)/fstab.tate
 RECOVERY_FSTAB_VERSION = 2
