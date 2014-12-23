7a16f819a80b36f9e7d6bcf705244d92100ef31b
Hashcode <hashcode0f@gmail.com>
2014-12-23 14:06:33 -0800

    tate: update kernel defconfigs and cmdline
    Change-Id: Iad4ae7ffcf3123f12b9dca1b6c6eceda0dcaa6eb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,8 +23,9 @@ TARGET_BOARD_OMAP_CPU := 4460
 
 # Kernel Build
 TARGET_KERNEL_SOURCE := kernel/amazon/bowser-common
-TARGET_KERNEL_CONFIG := tate_android_defconfig
-BOARD_KERNEL_CMDLINE := mem=1G console=/dev/null rootdelay=2 init=/init androidboot.console=ttyO2 androidboot.hardware=bowser androidboot.selinux=permissive
+TARGET_KERNEL_CONFIG := android_omap4_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := android_tate_defconfig
+BOARD_KERNEL_CMDLINE := mem=1G rootdelay=2 init=/init androidboot.console=ttyO2 androidboot.hardware=bowser androidboot.selinux=permissive
 
 # External SGX Module
 SGX_MODULES:
